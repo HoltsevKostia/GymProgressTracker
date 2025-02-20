2f3e73ac385d6ef579ca8dc71a48362cb0b005f9
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-02-20 21:21:48 +0200

    Add mapping for User. Add UserRepository and UserService

---
+++ GymProgressTracker.Server/Services/User/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using GymProgressTracker.Server.Models.DTO.User;
+
+namespace GymProgressTracker.Server.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserDTO?> GetUserByIdAsync(int id);
+        Task<UserDTO> RegisterAsync(AddUserDTO userDTO);
+        Task<UserDTO> LoginAsync(LoginUserDTO userDTO);
+        string HashPassword(string password);
+    }
+}
---
+++ GymProgressTracker.Server/Services/User/UserService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using GymProgressTracker.Server.Models.DTO.User;
+using GymProgressTracker.Server.Repositories.User;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GymProgressTracker.Server.Services.User
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDTO?> GetUserByIdAsync(int id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+            return user == null ? null : _mapper.Map<UserDTO>(user);
+        }
+
+        public string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            return Convert.ToBase64String(sha256.ComputeHash(Encoding.UTF8.GetBytes(password)));
+        }
+
+        public async Task<UserDTO> LoginAsync(LoginUserDTO loginUserDTO)
+        {
+            var user = await _userRepository.GetByEmailAndPassword(loginUserDTO.Email, loginUserDTO.Password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var userDTO = _mapper.Map<UserDTO>(user);
+            return userDTO;
+        }
+
+        public async Task<UserDTO> RegisterAsync(AddUserDTO userDTO)
+        {
+            var isEmailTaken = await _userRepository.IsEmailTaken(userDTO.Email);
+            if (isEmailTaken)
+            {
+                return null;
+            }
+
+            var user = _mapper.Map<Models.Domain.User>(userDTO);
+            user.Password = HashPassword(userDTO.Password);
+
+            await _userRepository.AddAsync(user);
+
+            var userDTOResult = _mapper.Map<UserDTO>(user);
+
+            return userDTOResult;
+        }
+    }
+}
