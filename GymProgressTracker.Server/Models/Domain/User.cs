9fd880b9e29d458ee5d2d733c97fe89f848df086
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-02-20 19:21:28 +0200

    Add base repository with CRUD. Add DTO for Domain tables.

--- GymProgressTracker.Server/Models/Domain/User.cs
+++ GymProgressTracker.Server/Models/Domain/User.cs
@@ -7,11 +7,8 @@ namespace GymProgressTracker.Server.Models.Domain
     public class User
     {
         public int Id { get; set; }
-
         public string Username { get; set; } = string.Empty;
-
         public string Email { get; set; } = string.Empty;
-
         public string Password { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; }
